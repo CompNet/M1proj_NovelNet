b3aa962f75f206288c32e03bd5a48ed70ab58df4
LemaireTewis <57706035+LemaireTewis@users.noreply.github.com>
2020-12-23 23:36:46 +0100

    maj graph

--- src/pipeline/CreateGraph.java
+++ src/pipeline/CreateGraph.java
@@ -1,35 +1,34 @@
 package pipeline;
 
-import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 
 public class CreateGraph {
 	public void graphC(CooccurrenceTable tab,Graph graph) {
-		Map<String,Node> charMapA = new HashMap<String, Node>(); // Map of Nodes with their IDs		
+		List<Node> nodeListA = new LinkedList<Node>();
+		List<Node> nodeListB = new LinkedList<Node>();
  		List<Edge> edgeList = new LinkedList<Edge>(); // List of edges
+ 		int cpt = 0;
 		for (int i=0; i<tab.listCharA.size(); i++){ // Until we reach the size of the list of sentences
 			Node nA = new Node(tab.listCharA.get(i),tab.listCharA.get(i),0); // We create a new node
-			charMapA.put(nA.id, nA); // Add it to the map
-			charMapA.get(tab.listCharA.get(i)).addWeight((1f/tab.listDistanceWord.get(i))); //If weighting is true, we add weight to the mention in the map
+			Node nB = new Node(tab.listCharB.get(i),tab.listCharB.get(i),0); // We create a new node
+			nodeListA.add(nA);
+			nodeListB.add(nB);
+			nodeListA.get(i).addWeight((1f/tab.listDistanceWord.get(i))); //If weighting is true, we add weight to the mention in the map
 		}
-		for (int i=0; i<tab.listCharA.size(); i++){
-			for (Node nodeL : charMapA.values()){ // For each left node
-				for (Node nodeR : charMapA.values()){ // For each right node
-					Edge edge = new Edge(nodeL.id,nodeL,nodeR,false,nodeL.weight); // We create a new edge which links the two nodes with their weight
-					if (tab.listCharA.get(i) == nodeL.getName() && tab.listCharB.get(i) == nodeR.getName()) edgeList.add(edge); // If the list does not already contain the edge, be it reversed or not, we add it to the list
-				}
-			}
+		while (cpt != nodeListA.size()){
+			Edge edge = new Edge(nodeListA.get(cpt).id,nodeListA.get(cpt),nodeListB.get(cpt),false,nodeListA.get(cpt).weight); // We create a new edge which links the two nodes with their weight
+			edgeList.add(edge); // If the list does not already contain the edge, be it reversed or not, we add it to the list
+			cpt++;
 		}
-		for (Node n : charMapA.values()) { // For each node in the map
+		for (Node n : nodeListA) { // For each node in the map
 			graph.addNode(n); // We add it to the graph
 		}
 		for (Edge e : edgeList) { // For each edge in the list
-			graph.addEdgeWithWeighting(e); // We add it to the graph with weighting
+			graph.addEdgeWithWeighting(e); // We add it to the graph with weighting			
 		}
-		System.out.println(charMapA.toString());
-		charMapA.clear(); //We clear the map of nodes
 		edgeList.clear(); // We clear the list of edges
+		nodeListA.clear();
+		nodeListB.clear();
 	}
 }
