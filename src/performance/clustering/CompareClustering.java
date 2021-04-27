d557d992d4118aff220d2799a0f4e0ffd2950d43
Baptiste Quay <baptiste.quay@gmail.com>
2021-04-27 11:10:53 +0200

    adition of smile library + changed clustering eval

--- src/performance/clustering/CompareClustering.java
+++ src/performance/clustering/CompareClustering.java
@@ -1,13 +1,10 @@
 package performance.clustering;
 
 import java.io.IOException;
-import java.lang.ref.Reference;
 import java.util.Comparator;
 
-import jsat.classifiers.CategoricalData;
-import jsat.classifiers.ClassificationDataSet;
-import jsat.clustering.evaluation.AdjustedRandIndex;
-import jsat.linear.Vec;
+import smile.validation.metric.AdjustedRandIndex;
+
 import novelnet.util.CustomCorefChain;
 
 public class CompareClustering {
@@ -18,23 +15,34 @@ public class CompareClustering {
     ClusterContainer reference;
 
     /**
-	 * the container with the best clusters found by the engine
+	 * the container with the best clusters found by the clustering engine
 	*/
     ClusterContainer bestClusters;
 
-    /*
-     * precision for the best clusters
+    /**
+     * precision for the best clusters should be between -1.0 and 1.0 (0 = random, 1.0 = perfect, -1.0 worse than random)
     */
     double precision;
 
-    /*
+    /**
      * dbScan distance used to find the best clusters
     */
     double dbScanDist;
 
+    /**
+     * empty class constructor
+    */
     public CompareClustering() {
+        this.bestClusters = null;
+        this.precision = -2.0;
+        this.dbScanDist = 0.0;
     }
 
+    /**
+     * class constructor specifying the reference data
+     * 
+     * @param reference the ClusterContainer with the reference data
+    */
     public CompareClustering(ClusterContainer reference) {
         this.reference = reference;
         this.bestClusters = null;
@@ -74,6 +82,12 @@ public class CompareClustering {
         this.dbScanDist = dbScanDist;
     }
 
+    /**
+     * CompareClustering builder from .xml file
+     * 
+     * @param path the path to the xml file containing the reference data
+     * @return the ComparaClusturing object
+    */
     public static CompareClustering buildFromXML(String path) throws IOException{
         CompareClustering result = new CompareClustering();
         result.setReference(ClusterContainer.buildClusterContainerFromXML(path));
@@ -81,38 +95,51 @@ public class CompareClustering {
         return result;
     }
 
+    /**
+     *  evaluation function :
+     *      cluster with the specified distance,
+     *      transform data into arrays,
+     *      compare the arrays
+     * 
+     * <br><br> result will be stocked into bestClusters
+     * 
+     * @param dbScanDist the distance with wich you want to run the dbscan algorithm
+     * @return the precision for the dbscan clusturing algorithm with the specified distance
+    */
     public double evaluate(double dbScanDist){
-
         ClusterContainer clusteredData = reference.clusterization(dbScanDist);
 
-        reference.getCorefChains().sort(Comparator.comparing(CustomCorefChain::getId));
+        reference.getCorefChains().sort(Comparator.comparing(CustomCorefChain::getId)); //making sure reference and data are in the same order
         clusteredData.getCorefChains().sort(Comparator.comparing(CustomCorefChain::getId));
 
-        //Creation of the dataset From clusteredData
-        ClassificationDataSet cds = new ClassificationDataSet(1,
-            new CategoricalData[0],
-            new CategoricalData(clusteredData.getLastCluster()+1)   //max number of clusters
-        );
+        //putting the data into an array
+        int[] data = new int[clusteredData.getCorefChains().size()];
 
-        //adding the coref chains into the data set
+        //adding the coref chains into the data
+        int i = 0;
         for(CustomCorefChain ccc : clusteredData.getCorefChains()){
-            cds.addDataPoint(Vec.random(1), new int[0], ccc.getClusterID());
+            data[i] = ccc.getClusterID();
+            i++;
         }
 
         //putting the reference into an array
-        int[] d = new int[reference.getCorefChains().size()];
+        int[] ref = new int[reference.getCorefChains().size()];
 
-        //adding the coref chains into the data set
-        int i = 0;
+        //adding the coref chains into the reference
+        i = 0;
         for(CustomCorefChain ccc : reference.getCorefChains()){
-            d[i] = ccc.getClusterID();
+            ref[i] = ccc.getClusterID();
             i++;
         }
 
         AdjustedRandIndex ari = new AdjustedRandIndex();
-        double score = ari.evaluate(d, cds);
-        //conver score to ARI score
-        score = 1.0-score;
+        
+        //there is a bug in dbscan : a node can be in 2 clusters.
+        //We didn't know how to fix it so when there is this bug we make sure the score is very low and not counted.
+        double score = -3.0;
+        if (ref.length == data.length){
+            score = ari.score(ref, data);    //order of arrays shouldn't matter
+        }
 
         if (score > precision){
             precision = score;
@@ -123,6 +150,13 @@ public class CompareClustering {
         return score;
     }
 
+    /**
+     * simple function to find the best clustering for dbscan algorithm, the result will be stocked into bestClusters.
+     * 
+     * @param begin first distance tried (0.2 is the recomended value)
+     * @param end loop will not go higher than this distance (this should be higher than the begin argument, 0.61 is the recomended value)
+     * @param increment increment for the loop (0.05 is the recomended value, reduce to be more precise, augment to be faster)
+    */
     public void findBestCluster(double begin, double end, double increment){
 
         for(double i = begin; i <=end; i+= increment){
@@ -137,7 +171,7 @@ public class CompareClustering {
         bestClusters.displayByCluster();
 
         System.out.println("Precision : " + getPrecision());
-        System.out.println("dbscan dsit : " + getDbScanDist() + "\n");
+        System.out.println("dbscan distance : " + getDbScanDist() + "\n");
 
     }
 
@@ -148,49 +182,62 @@ public class CompareClustering {
 
     // TESTS
 
-    private static void testJsat(){
-        //using example from http://www.otlet-institute.org/wikics/Clustering_Problems.html
-        ClassificationDataSet cds = new ClassificationDataSet(1, new CategoricalData[0], new CategoricalData(3));
-        for(int i = 0; i < 3; i++)
-            for(int j = 0; j < 3; j++)
-                cds.addDataPoint(Vec.random(1), new int[0], i);
-        int[] d = new int[9];
-        d[0] = d[1] = 0;
-        d[2] = d[3] = d[4] = d[5] = 1;
-        d[6] = d[7] = 2;
-        d[8] = 3;
-        
-        AdjustedRandIndex ari = new AdjustedRandIndex();
-        double score = ari.evaluate(d, cds);
-        //conver tot ARI
-        score = 1.0-score;
-        System.out.println("should be around 0.46 : " + score);
+    
+    public static void main(String[] args) throws IOException {
+        String language = "en";
+        String fileName = "HarryPotter3_ShriekingShack";
+        String path = "res/manualAnnotation/ner_coref_clustering/" + language + "/" + fileName + ".xml";
 
-        //test 2
+        //testSmile();
 
-        cds = new ClassificationDataSet(1, new CategoricalData[0], new CategoricalData(4));
-        cds.addDataPoint(Vec.random(1), new int[0], 0);
-        cds.addDataPoint(Vec.random(1), new int[0], 0);
-        cds.addDataPoint(Vec.random(1), new int[0], 1);
-        cds.addDataPoint(Vec.random(1), new int[0], 1);
-        cds.addDataPoint(Vec.random(1), new int[0], 1);
-        cds.addDataPoint(Vec.random(1), new int[0], 1);
-        cds.addDataPoint(Vec.random(1), new int[0], 2);
-        cds.addDataPoint(Vec.random(1), new int[0], 2);
-        cds.addDataPoint(Vec.random(1), new int[0], 3);
+        //testEvaluation(path);
+
+        testBestClusters(path);
+    }
+
+    private static void testSmile(){
+        smile.validation.metric.AdjustedRandIndex ari = new AdjustedRandIndex();
+        int[] ref = new int[9];
+        int[] data = new int[9];
+
+        //test1
+        ref[0] = ref[1] = 0;
+        ref[2] = ref[3] = ref[4] = ref[5] = 1;
+        ref[6] = ref[7] = 2;
+        ref[8] = 3;
+
+        data[0] = data[1] = 0;
+        data[2] = data[3] = data[4] = data[5] = 1;
+        data[6] = data[7] = 2;
+        data[8] = 3;
+
+        System.out.println("should be 1 : " + ari.score(ref, data));
+
+        //test2
+        ref[0] = ref[1] = 0;
+        ref[2] = ref[3] = ref[4] = ref[5] = 1;
+        ref[6] = ref[7] = 2;
+        ref[8] = 3;
+
+        data[0] = data[1] = 0;
+        data[2] = data[3] = data[4] = data[5] = 2;  //interchanging cluster number 1 and 2 but should still be 1
+        data[6] = data[7] = 1;
+        data[8] = 3;
+
+        System.out.println("should still be 1 : " + ari.score(ref, data));
 
 
-        d = new int[9];
-        d[0] = d[1] = 0;
-        d[2] = d[3] = d[4] = d[5] = 1;
-        d[6] = d[7] = 2;
-        d[8] = 3;
-        
-        ari = new AdjustedRandIndex();
-        score = ari.evaluate(d, cds);
-        //conver tot ARI
-        score = 1.0-score;
-        System.out.println("should be 1 : " + score);
+        //test3 is from http://www.otlet-institute.org/wikics/Clustering_Problems.html
+        ref[0] = ref[1] = 0;
+        ref[2] = ref[3] = ref[4] = ref[5] = 1;
+        ref[6] = ref[7] = 2;
+        ref[8] = 3;
+
+        data[0] = data[1] = data[2] = 0;
+        data[3] = data[4] = data[5] = 1;
+        data[6] = data[7] = data[8] = 2;
+
+        System.out.println("should be around 0.46 : " + ari.score(ref, data));        
     }
 
     private static void testEvaluation(String path) throws IOException{
@@ -208,24 +255,11 @@ public class CompareClustering {
     private static void testBestClusters(String path) throws IOException{
         ClusterContainer container = ClusterContainer.buildClusterContainerFromXML(path);
         CompareClustering compare = new CompareClustering(container);
-        compare.findBestCluster(0.20, 0.60, 0.05);
+        compare.findBestCluster(0.20, 0.61, 0.05);
 
         System.out.println("reference :");
         compare.getReference().displayByCluster();
 
         compare.displayResult();
     }
-
-    public static void main(String[] args) throws IOException {
-        String language = "en";
-        String fileName = "HarryPotter3_ShriekingShack";
-        String path = "res/manualAnnotation/ner_coref_clustering/" + language + "/" + fileName + ".xml";
-
-        //testJsat();
-
-        //testEvaluation(path);
-
-        testBestClusters(path);
-    }
-
 }
